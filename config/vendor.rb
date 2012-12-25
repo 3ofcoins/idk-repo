5aabb9bb43442ffbb67902fa8d628c8ea81e7a6b
Maciej Pasternacki <maciej@pasternacki.net>
2012-12-26 00:03:28 +0100

    Download also virtualenv.py, meh.

--- config/vendor.rb
+++ config/vendor.rb
@@ -1,5 +1,5 @@
 archive :virtualenv,
         :url => 'http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.8.4.tar.gz',
-        :version => '1.8.4' do
-  FileUtils::rm_rf Dir.entries('.')-%w|.. . AUTHORS.txt LICENSE.txt README.rst|
+        :version => '1.8.4-1' do
+  FileUtils::rm_rf Dir.entries('.')-%w|.. . AUTHORS.txt LICENSE.txt README.rst virtualenv.py|
 end
