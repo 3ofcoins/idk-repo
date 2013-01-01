a5f7105377edcc1bc74069e47214cddbdde1efba
Maciej Pasternacki <maciej@pasternacki.net>
2013-01-01 19:32:37 +0100

    Vendor chef-server cookbook.

--- config/vendor.rb
+++ config/vendor.rb
@@ -1,5 +1,9 @@
+require 'vendorificator/vendor/chef_cookbook'
+
 archive :virtualenv,
         :url => 'http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.8.4.tar.gz',
         :version => '1.8.4-1' do
   FileUtils::rm_rf Dir.entries('.')-%w|.. . AUTHORS.txt LICENSE.txt README.rst virtualenv.py|
 end
+
+chef_cookbook 'chef-server'
