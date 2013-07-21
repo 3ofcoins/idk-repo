5d856da6db88ffdb39ab08e6e2e51c8922f30e73
Maciej Pasternacki <maciej@3ofcoins.net>
2013-07-21 22:56:40 +0200

    Vendor only cookbooks. Maybe we should skip that as well?

--- config/vendor.rb
+++ config/vendor.rb
@@ -1,9 +1,2 @@
-require 'vendorificator/vendor/chef_cookbook'
-
-archive :virtualenv,
-        :url => 'http://pypi.python.org/packages/source/v/virtualenv/virtualenv-1.8.4.tar.gz',
-        :version => '1.8.4-1' do
-  FileUtils::rm_rf Dir.entries('.')-%w|.. . AUTHORS.txt LICENSE.txt README.rst virtualenv.py|
-end
 
 chef_berkshelf
