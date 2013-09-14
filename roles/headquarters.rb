154e0dc86e3d3790a801bcc76a0ab25025850545
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-15 00:57:12 +0200

    Initial HQ, with odin authorizer & chef server

---
+++ roles/headquarters.rb
@@ -0,0 +1,44 @@
+name "headquarters"
+description "Heart of the operation"
+
+run_list 'role[__base]',
+         'recipe[ssl-key-vault]',
+         'recipe[chef-server-webapp]',
+         'recipe[apache2_odin_auth::server]'
+
+certificates_path = Pathname.new(__FILE__).dirname.dirname.
+  join('config/certificates')
+
+default_attributes(
+  :apache2 => {
+    :odin_auth => {
+      :server_branch => 'develop',
+      :server_config => {
+        :google_apps_domain => '3ofcoins.net',
+        :company => 'Three of Coins',
+        :company_url => 'http://3ofcoins.net/',
+        'template-variables' => {
+          :links => [
+            'https://www.hipchat.com/google/login?from=google&domain=3ofcoins.net'
+          ]}},
+      :server_domain => 'i.3ofcoins.net',
+      :server_ssl_certificate_path => '/etc/ssl/certs/star.i.3ofcoins.net.crt',
+      # :server_ssl_certificate_chain_path => '/etc/ssl/certs/codility.net.chain.pem',
+      :server_ssl_key_path => '/etc/ssl/private/star.i.3ofcoins.net.key'
+    }},
+  :'chef-server' => {
+    :api_fqdn => 'chef.i.3ofcoins.net',
+    :configuration => {
+      :chef_server_webui => {:enable => false },
+      :notification_email => 'maciej@3ofcoins.net'},
+    :webapp => {
+      :ssl_key_path => '/etc/ssl/private/star.i.3ofcoins.net.key',
+      :ssl_certificate_path => '/etc/ssl/certs/star.i.3ofcoins.net.crt' # ,
+      # :ssl_certificate_chain_path => '/etc/ssl/certs/star.i.3ofcoins.net.chain.pem'
+    }},
+  :sanitize => { :ports => { :http => true, :https => true }},
+  :ssl_certificates => {
+    'star.i.3ofcoins.net' => {
+      'crt' => certificates_path.join('star.i.3ofcoins.net.crt').read
+    }}
+)
