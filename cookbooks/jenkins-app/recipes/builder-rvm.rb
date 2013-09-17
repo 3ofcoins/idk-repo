275eb0ce4ce453929c5d52750ec1e5afbe153a41
Maciej Pasternacki <maciej@3ofcoins.net>
2013-09-17 12:40:09 +0200

    RVM builder recipe

---
+++ cookbooks/jenkins-app/recipes/builder-rvm.rb
@@ -0,0 +1,19 @@
+# RVM's requirements
+package 'gawk'
+package 'libreadline6-dev'
+package 'zlib1g-dev'
+package 'libssl-dev'
+package 'libyaml-dev'
+package 'libsqlite3-dev'
+package 'sqlite3'
+package 'libgdbm-dev'
+package 'libncurses5-dev'
+package 'libtool'
+package 'pkg-config'
+package 'libffi-dev'
+package 'unzip'
+
+# chef-browser etc needs nokogiri
+package 'libxml2-dev'
+package 'libxslt1-dev'
+package 'acl'
