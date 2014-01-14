f96c9df7481a9e43f01c1a3d741506f45e493736
Maciej Pasternacki <maciej@3ofcoins.net>
2014-01-14 19:23:26 +0100

    Update base role

--- roles/base.rb
+++ roles/base.rb
@@ -1,13 +1,29 @@
-name "__base"
-description "Base role applied to all nodes. It should be included in the base role of a concrete deployment."
+name "base"
+description "Base role applied to all nodes."
 
 run_list 'recipe[apt]',
+         'recipe[ohai]',
          'recipe[omnibus_updater]',
          'recipe[chef-client::config]',
-         'recipe[sanitize]'
+         'recipe[sudo]',
+         'recipe[users::sysadmins]',
+         'recipe[sanitize]',
+         'recipe[hostname]'
 
 default_attributes(
+  :authorization => {
+    :sudo => {
+      :agent_forwarding => true,
+      :groups => ['sysadmin'],
+      :include_sudoers_d => true,
+      :passwordless => true}},
+  :domain => $realm.domain,
   :omnibus_updater => {
     :version => '11.8.2',
     :remove_chef_system_gem => true,
-    :cache_dir => '/var/cache/chef' })
+    :cache_dir => '/var/cache/chef' },
+:openssh => ,
+  :sanitize => {
+    :keep_access => !::Dir[$realm.path('data_bags/users/*.json')].empty?
+  },
+  :set_fqdn => "*.#{$realm.domain}")
