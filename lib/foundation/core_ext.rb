3bf6ae1e0b9ec1c5770e76a8882d8e971419632e
Maciej Pasternacki <maciej@3ofcoins.net>
2014-01-14 20:47:36 +0100

    moar core ext

--- lib/foundation/core_ext.rb
+++ lib/foundation/core_ext.rb
@@ -5,6 +5,21 @@ module Foundation
       basename ||= ::File.basename(caller, '.rb')
       $realm.configure(self, basename, caller)
     end
+
+    def instance_load(path)
+      caller = ::Kernel.caller.first.sub(/(:\d+)?(:in .*)?$/, '')
+      path = ::File.expand_path(path, ::File.dirname(caller))
+      self.instance_eval(::File.read(path), path)
+    end
+
+    def instance_load_subdir
+      caller = ::Kernel.caller.first.sub(/(:\d+)?(:in .*)?$/, '')
+      glob = ::File.join(::File.dirname(caller),
+        ::File.basename(caller, '.rb'), '*.rb')
+      ::Dir[glob].sort.each do |subconf|
+        instance_load(::File.expand_path(subconf))
+      end
+    end
   end
 end
 
@@ -17,5 +32,23 @@ if defined?(Chef::Config)
     class << self
       include Foundation::ConfigureMixin
     end
+  end
+end
+
+if defined? Chef::Role
+  class Chef::Role
+    include Foundation::ConfigureMixin
+  end
+end
+
+if defined? Chef::Environment
+  class Chef::Environment
+    include Foundation::ConfigureMixin
+  end
+end
+
+if defined? Chef::Cookbook::Metadata
+  class Chef::Cookbook::Metadata
+    include Foundation::ConfigureMixin
   end
 end
