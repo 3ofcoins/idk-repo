c56b2d9b1f5d1de48a9b93e1de270fd9be6d3725
Maciej Pasternacki <maciej@3ofcoins.net>
2014-02-24 16:14:09 +0100

    Typo.

--- config/solo.rb
+++ config/solo.rb
@@ -5,7 +5,7 @@ $:.unshift(_libdir) unless $:.include?(_libdir)
 require 'idk/config'
 
 file_cache_path File.join(_basedir, 'tmp/cache')
-cookbook_path [ File.join(_basedir, 'cookbooks', File.join(_basedir, 'vendor/cookbooks') ]
+cookbook_path [ File.join(_basedir, 'cookbooks'), File.join(_basedir, 'vendor/cookbooks') ]
 data_bag_path File.join(_basedir, 'data_bags')
 verbose_logging false
 
