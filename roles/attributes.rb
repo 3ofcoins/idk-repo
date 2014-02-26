ee96c81868788950e5d82d571fa77c3f0244ec87
Maciej Pasternacki <maciej@analyticsfire.com>
2014-02-26 18:47:06 +0000

    commented out rvm upgrade

--- roles/attributes.rb
+++ roles/attributes.rb
@@ -35,7 +35,8 @@ default_attributes(
       :loglevel => 'notice'
     }},
   :rvm => {
-    :default_ruby => "system"
+    :default_ruby => "system",
+    # :upgrade => "stable",
   },
   :sanitize => {
     :locale => {
