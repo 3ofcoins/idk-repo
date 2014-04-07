5686d4d798b087037a572ddb0240134aebe01cce
Maciej Pasternacki <maciej@3ofcoins.net>
2014-04-07 15:12:08 +0200

    Run chef-solo without idk/config too

--- config/solo.rb
+++ config/solo.rb
@@ -2,7 +2,11 @@ _basedir = File.expand_path(File.dirname(File.dirname(__FILE__)))
 
 _libdir = File.join(_basedir, 'lib')
 $:.unshift(_libdir) unless $:.include?(_libdir)
-require 'idk/config'
+begin
+  require 'idk/config'
+rescue Exception => e
+  warn "Can't require idk/config (#{e}), continuing without"
+end
 
 file_cache_path File.join(_basedir, 'tmp/cache')
 cookbook_path [ File.join(_basedir, 'cookbooks'), File.join(_basedir, 'vendor/cookbooks') ]
