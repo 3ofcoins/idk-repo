a5f78e9d17294ad3099aad28d37b74407a307e7a
Marta Paciorkowska <marta@example.com>
2014-05-30 18:08:49 +0200

    [nfy] Almost done with Rake options

--- .chef/plugins/knife/do.rb
+++ .chef/plugins/knife/do.rb
@@ -3,7 +3,6 @@ require 'chef/knife'
 require 'rake'
 
 module KnifeTask
-
   Rake::TaskManager.record_task_metadata = true
 
   def load_rake_as_lib
@@ -37,47 +36,92 @@ module KnifeTask
       require 'rake'
     end
 
-    option :all,
-           short: '-A',
-           long: '--all',
-           description: 'Run all Rake tasks.',
-           proc: Proc.new { Rake.application.tasks.each { |t| Rake::Task[t.name].invoke } }
-
-    option :verbose,
-           short: '-v',
-           long: '--verbose',
-           description: 'Log message to standard output.',
-           proc: Proc.new { Kernel.system('rake --rakefile config/tasks.rb --verbose') }
-
-    # Options Rake magically captures from the command line.
-    # Defined here so they're returned by `knife do task --help`
+    # Options Rake magically captures from the command line. Defined here
+    # based on `rake -h` so they're returned by `knife do task --help`.
     option :execute_continue,
            short: '-E CODE',
+           long: '--execute-continue CODE',
            description: 'Execute some Ruby code, then continue with normal task processing.'
 
     option :execute_code,
            short: '-e CODE',
+           long: '--execute-code CODE',
            description: 'Execute some Ruby code and exit.'
 
     option :version,
            short: '-V',
+           long: '--version',
            description: 'Display Rake version.'
 
     option :dry_run,
            short: '-n',
+           long: '--dry-run',
            description: 'Do a dry run without executing actions.'
 
     option :execute_print,
-           short: '-p',
+           short: '-p CODE',
+           long: '--execute-print CODE',
            description: 'Execute some Ruby code, print the result, then exit.'
 
     option :silent,
            short: '-s',
+           long: '--silent',
+           description: "Like --quiet, but also suppresses the 'in directory' announcement."
+
+    option :quiet,
+           short: '-q',
+           long: '--quiet',
            description: 'Do not log messages to standard output.'
 
     option :rakefile,
            short: '-f FILENAME',
+           long: '--rakefile FILENAME',
            description: 'Use FILENAME as the rakefile to search for.'
+
+    option :no_deprecation_warnings,
+           short: '-X',
+           long: '--no-deprecation-warnings',
+           description: 'Disable the deprecation warnings.'
+
+    option :system,
+           short: '-g',
+           long: '--system',
+           description: "Using system wide (global) rakefiles (usually '~/.rake/*.rake')."
+
+    option :no_system,
+           short: '-G',
+           long: '--no-system',
+           long: '--nosystem',
+           description: 'Use standard project Rakefile search paths, ignore system wide rakefiles.'
+
+    option :trace,
+           short: '-t',
+           long: '--trace=[OUT]',
+           description: "Turn on invoke/execute tracing, enable full backtrace. OUT can be stderr (default) or stdout."
+
+    option :rules,
+           long: '--rules',
+           description: 'Trace the rules resolution.'
+
+    option :libdir,
+           short: '-I LIBDIR',
+           long: '--libdir LIBDIR',
+           description: 'Include LIBDIR in the search path for required modules.'
+
+    option :require,
+           short: '-r MODULE',
+           long: '--require MODULE',
+           description: 'Require MODULE before executing rakefile.'
+
+    option :suppress_backtrace,
+           long: '--suppress-backtrace PATTERN',
+           description: 'Suppress backtrace lines matching regexp PATTERN. Ignored if --trace is on.'
+
+    option :rakelib,
+           short: '-R RAKELIBDIR',
+           long: '--rakelibdir RAKELIBDIR',
+           long: '--rakelib RAKELIBDIR',
+           description: "Auto-import any .rake files in RAKELIBDIR. (default is 'rakelib')"
 
     def run
       load_rake_as_lib
